696395d46a165a37ad90620f4e1e158bb29294ef
NoaShtober <94970175+NoaShtober@users.noreply.github.com>
2023-05-01 15:30:04 +0300

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://www.calculator.net/")
+driver.maximize_window()
+user_name = driver.find_element(By.CLASS_NAME, "cbmark")
+
+
